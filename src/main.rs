86e6d5a4e78aadfbf55cc962f815f5f7e1cd0ed0
Vincent <bsvtgc@gmail.com>
2022-07-05 16:54:48 -0400

    Map trap handler and stack pointer

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
 
 #![no_std]
 #![no_main]
-#![allow(dead_code)]
 
 use core::panic::PanicInfo;
 use core::arch::asm;
+
+/************** PANIC HANDLER ****************/
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
+/************** TRAP HANDLER ****************/
+
 #[no_mangle]
 #[link_section = ".mtvec_base"]
-fn trap_handler() {
-
-    unsafe {
-        asm!("nop");
-        asm!("csrr t0, mcause;");
-        asm!("nop");
-    }
-
+ fn trap_handler() -> ! {
+    loop{}
 }
+
+/********** ENTRY FUNCTION ******************/
 
 #[no_mangle]
 pub fn _start() {
-    type FnPtr = fn() -> ();
+
+    extern "C" {
+        static  _stack_start: u32;
+    }
+
+    type FnPtr = fn() -> !;
     let th: FnPtr = trap_handler;
 
     unsafe{
-    asm!("csrw mtvec, {}" ,
-        in(reg) th);
+
+        let sp = &_stack_start;
+
+        asm!("csrw mtvec, {}" ,
+            in(reg) th);
+
+        asm!("mv sp, {}" ,
+            in(reg) sp);
     }
 
     loop {}
